4355a6e9fe7f2b3c458de1b427b6844db95ff030
FloRad <florad92@gmail.com>
2021-10-11 20:02:30 +0200

    updated packages

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,6 @@
 import { Dice3D } from './interfaces/DiceSoNice';
 import SwadeGame from './interfaces/SwadeGame';
-import { SWADE } from './module/config';
+import { SwadeConfig } from './module/config';
 
 declare global {
   interface Game {
@@ -13,6 +13,6 @@ declare global {
   }
 
   interface CONFIG {
-    SWADE: typeof SWADE;
+    SWADE: SwadeConfig;
   }
 }
