0dedab3f144ad14f062a27747e6c5acd66baf165
FloRad <florad92@gmail.com>
2021-12-19 00:59:35 +0100

    fixed CardMetadata type

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -20,7 +20,7 @@ declare global {
 
 export type ActorMetadata = CompendiumCollection.Metadata & { entity: 'Actor' };
 export type ItemMetadata = CompendiumCollection.Metadata & { entity: 'Item' };
-export type CardMetadata = CompendiumCollection.Metadata & { entity: 'Item' };
+export type CardMetadata = CompendiumCollection.Metadata & { entity: 'Card' };
 export type JournalMetadata = CompendiumCollection.Metadata & {
   entity: 'JournalEntry';
 };
