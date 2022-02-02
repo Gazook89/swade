86346fef5820bb2ef2aad9aa70218d1438f43c9f
Kristian Serrano <kristianserrano@gmail.com>
2022-02-02 15:36:13 -0500

    Renames enums for expirations

--- src/module/enums/StatusEffectExpirationsEnums.ts
+++ src/module/enums/StatusEffectExpirationsEnums.ts
@@ -1,6 +1,6 @@
 export enum StatusEffectExpiration {
-  BEGINNING_OF_TURN_AUTO = 0,
-  BEGINNING_OF_TURN_PROMPT = 1,
-  END_OF_TURN_AUTO = 2,
-  END_OF_TURN_PROMPT = 3,
+  StartOfTurnAuto = 0,
+  StartOfTurnPrompt = 1,
+  EndOfTurnAuto = 2,
+  EndOfTurnPrompt = 3,
 }
