d18f2aeff85a53fc5d4512c207a3fb906da4a93c
FloRad <florad92@gmail.com>
2022-02-22 23:16:31 +0100

    improved build plugin

--- tools/rollup-plugin-build-compendiums.js
+++ tools/rollup-plugin-build-compendiums.js
@@ -1,24 +1,30 @@
 import path from 'node:path';
 import { readdir, readFile, writeFile, mkdir } from 'node:fs/promises';
 import { load as yamlLoad } from 'js-yaml';
-import { existsSync } from 'node:fs';
+import { existsSync, readdirSync } from 'node:fs';
 
 /**
  * //TODO possibly revisit for watch functionality
  * @param {object} options
  * @param {string} options.src - The source directory which contains a subdirectory per compendium pack
  * @param {string} options.dest - The destination directory
+ * @returns {rollup.Plugin}
  */
 export default function buildCompendiums({ src, dest }) {
-  return {
+  /** @type {import('rollup').Plugin} */
+  const plugin = {
     name: 'build-compendiums',
-    async buildStart() {
+    load() {
+      getFiles(src).forEach((file) => this.addWatchFile(file));
+    },
+    async generateBundle() {
       //load the subdirectories
       const inputDir = path.resolve(src);
       const packs = await readdir(inputDir);
+      //go through each subdirectory
       for (const pack of packs) {
-        //go through each subdirectory
-        let dbContentString = '';
+        console.log(`Building pack ${pack}`);
+        let packData = '';
         const packPath = path.resolve(inputDir, pack);
         const entries = await readdir(packPath);
         //read each file in a subdirectory and push it into the array
@@ -29,25 +35,22 @@ export default function buildCompendiums({ src, dest }) {
           const content = yamlLoad(file, { filename: entry });
           //add ID if necessary
           if (!content._id) content._id = makeid();
-
           //add it to the complete DB string
-          dbContentString += JSON.stringify(content);
-          dbContentString += '\n';
+          packData += JSON.stringify(content);
+          packData += '\n';
         }
 
         //check if the output directory exists, and create it if necessary
         const outputDir = path.resolve(dest);
-        if (!existsSync(outputDir)) await mkdir(outputDir);
+        if (!existsSync(outputDir)) await mkdir(outputDir, { recursive: true });
 
         //write the contents to the pack file
-        await writeFile(
-          `${path.resolve(outputDir, pack)}.db`,
-          dbContentString,
-          { flag: 'w' },
-        );
+        const outputPath = path.resolve(outputDir, pack);
+        await writeFile(`${outputPath}.db`, packData, { flag: 'w' });
       }
     },
   };
+  return plugin;
 }
 
 function makeid(length = 16) {
@@ -59,4 +62,18 @@ function makeid(length = 16) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
+}
+
+/**
+ * Recursively reads a directory to get all filepaths in that directory
+ * @param {string} dir - The path to the directory that should be explored
+ * @returns {Array<string>} a list of filepaths
+ */
+function getFiles(dir) {
+  const subdirs = readdirSync(dir, { withFileTypes: true });
+  const files = subdirs.map((subdir) => {
+    const res = path.resolve(dir, subdir.name);
+    return subdir.isDirectory() ? getFiles(res) : res;
+  });
+  return files.reduce((a, f) => a.concat(f), []);
 }
