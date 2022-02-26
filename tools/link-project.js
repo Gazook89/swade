692d2a745dd009a51af6d34b948f94200243f429
FloRad <florad92@gmail.com>
2022-02-26 19:38:17 +0100

    added yargs help to project linkking script

--- tools/link-project.js
+++ tools/link-project.js
@@ -7,7 +7,13 @@ import { hideBin } from 'yargs/helpers';
 
 /** Link build to User Data folder */
 
-const argv = yargs(hideBin(process.argv)).parse();
+const argv = await yargs(hideBin(process.argv))
+  .option('c', {
+    alias: 'clean',
+    describe: 'remove the link instead of creating one',
+  })
+  .help()
+  .parse();
 const config = JSON.parse(readFileSync('foundryconfig.json', 'utf-8'));
 const name = 'swade';
 const destDir = 'systems';
