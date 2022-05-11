7b8417e9a1c851c754db6d314fbfe9faccdeb16f
FloRad <florad92@gmail.com>
2022-05-11 23:21:19 +0200

    added comments to advances interface

--- src/interfaces/Advance.ts
+++ src/interfaces/Advance.ts
@@ -3,8 +3,10 @@ import { constants } from '../module/constants';
 export interface Advance {
   type: ValueOf<typeof constants.ADVANCE_TYPE>;
   notes: string;
+  /** the place of this particular advance in the track */
   sort: number;
   planned: boolean;
   id: string;
+  /** only used for display */
   rank?: ValueOf<typeof constants.RANK>;
 }
